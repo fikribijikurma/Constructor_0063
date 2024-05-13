2e34513061d5b9dcd696a802a653c3ddb1d9a522
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-05-13 01:37:02 -0700

    menambahkan metode

---
+++ constuktor.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa
+{
+public:
+    int nim;
+    string nama;
+
+    Mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
+    Mahasiswa(int iNim)
+    {
+        nim = iNim;
+    };
+    Mahasiswa(string);
+    Mahasiswa(int INim, string INama);
+    void cetak();
+};
+
+
--- mbrStaticfunc.cpp
+++ mbrStaticfunc.cpp
@@ -14,7 +14,7 @@ public:
     void setId();
     void printAll();
 
-    static void setNim(int pNim) { nim = pnim; }
+    static void setNim(int pNim) { nim = pNim; }
     static int getNim() { return nim; }
 
     mahasiswa (string pnama):nama(pnama)
@@ -35,4 +35,23 @@ void mahasiswa::printAll()
     cout << " ID     = " << id << endl;
     cout << " nama   = " << nama << endl;
     cout << endl;
+}
+
+int main()
+{
+    mahasiswa mhs1("Sri Dadi");
+    mahasiswa mhs1("Budi Jatmiko");
+
+    mahasiswa::setNim(9); // memaksa nim melalui static member function "setNim" 
+    mahasiswa mhs1("Andi Janu");
+    mahasiswa mhs1("Joko Wahono");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    cout << "akses dari luar object = " << mahasiswa::getNim() << endl; // mengakses nim
+
+    return 0;
 }
