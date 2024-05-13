74ab5e8f628153af7494e7a8e4a3f872f7a5a13e
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-05-13 01:08:49 -0700

    memasukan variabel dari nim mahaiswa

--- mbrStaticfunc.cpp
+++ mbrStaticfunc.cpp
@@ -4,5 +4,24 @@ using namespace std;
 
 class mahasiswa
 {
-    
-}
+private:
+    static int nim;
+
+public:
+    int id;
+    string nama;
+
+    void setId();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pnim; }
+    static int getNim() { return nim; }
+
+    mahasiswa (string pnama):nama(pnama)
+    {
+        setId();
+    }
+};
+
+int mahasiswa::nim = 100;
+
