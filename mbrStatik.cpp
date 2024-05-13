8a44b391c823f0f3d3b92643a5ec50b2b2f0c1b4
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-05-13 00:50:17 -0700

    menampilkan objek

--- mbrStatik.cpp
+++ mbrStatik.cpp
@@ -1,4 +1,4 @@
-#inclaude <iostream>
+#include <iostream>
 using namespace std;
 
 class mahasiswa
@@ -18,3 +18,23 @@ public:
 };
 
 int mahasiswa::nim = 20;
+
+void mahasiswa::setId()
+{
+    id  = nim++;
+}
+
+void mahasiswa::printAll()
+{
+    cout << "ID   = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main()
+{
+    mahasiswa mhs1("Lia Kurnia");
+    mahasiswa mhs2("Asroni");
+    mahasiswa mhs3("Andi Kurniawan");
+    mahasiswa mhs4("Joko purbo");
+}
